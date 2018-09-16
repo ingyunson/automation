b67bc3d57d552f2f78bdce5fd904d58c3b84c2f9
Ingyun Son <ingyunson@gmail.com>
2018-09-16 17:04:41 +0900

    Update: merge chatbot in the crawler

--- currency_crawler.py
+++ currency_crawler.py
@@ -1,6 +1,11 @@
 import urllib.request
 from bs4 import BeautifulSoup
-import chatbot_currency
+from datetime import datetime
+import telegram
+
+my_token = '620119228:AAFzcyGBnMYRxh22--x3_WprBVODyTdIcok' #botfather로 받은 토큰
+chatbot = telegram.Bot(token = my_token)
+chat_id = '@currencybot2018' #채널의 경우 @채널주소
 
 fp = urllib.request.urlopen('http://info.finance.naver.com/marketindex/exchangeList.nhn')
 source = fp.read()
@@ -19,11 +24,17 @@ for data in soup:
         money_key = None
         money_value = None
 
+now = datetime.now()
 currency_target = ['미국 USD', '유럽연합 EUR', '일본 JPY (100엔)', '중국 CNY', '홍콩 HKD', '대만 TWD']
+message = ['***오늘 '+ str('%s-%s-%s' % (now.year, now.month, now.day + 1)) + '의 환율은 다음과 같습니다.***']
 
 for key in money_data:
     if key in currency_target:
         value = money_data.get(key)
-        chatbot_currency.send_currency(key, value)
+        message.append(key + '의 환율은 ' + str(value) + '원입니다.')
     else:
         pass
+
+text = "\n".join(message)
+
+chatbot.sendMessage(chat_id = chat_id, text = text)
