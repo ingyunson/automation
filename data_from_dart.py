3c579eea6cc2b13a955564cef4764fc6d0ae1223
Ingyun Son <ingyunson@gmail.com>
2018-10-14 11:03:38 +0900

    Uodate: save attached file and get finalcial info from file

--- data_from_dart.py
+++ data_from_dart.py
@@ -1,18 +1,25 @@
 import requests
 import json
 from pandas.io.json import json_normalize
+import pandas as pd
+import os
 import re
 
 auth = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 headers={'Referer':'https://dart.fss.or.kr/dsap001/guide.do'}
 
+pdf_link_tmpl = "http://dart.fss.or.kr/pdf/download/pdf.do?rcp_no={rcp_no}&dcm_no={dcm_no}"
+excel_link_tmpl = "http://dart.fss.or.kr/pdf/download/excel.do?rcp_no={rcp_no}&dcm_no={dcm_no}&lang=ko"
+ifrs_link_tmpl = "http://dart.fss.or.kr/pdf/download/ifrs.do?rcp_no={rcp_no}&dcm_no={dcm_no}&lang=ko"
+
 #오늘(혹은 전일) 공시 100건
-recent_tmpl = 'http://dart.fss.or.kr/api/search.json?auth={auth}&page_set=100'
-recent= recent_tmpl.format(auth=auth)
-rc = requests.get(recent, headers = headers)
-today_json = json.loads(rc.text)
-today_result = json_normalize(today_json, 'list')
-#print(today_result)
+def today_announce():
+    recent_tmpl = 'http://dart.fss.or.kr/api/search.json?auth={auth}&page_set=100'
+    recent= recent_tmpl.format(auth=auth)
+    rc = requests.get(recent, headers = headers)
+    today_json = json.loads(rc.text)
+    today_result = json_normalize(today_json, 'list')
+    print(today_result)
 
 
 #특정 회사 공시
@@ -27,6 +34,7 @@ result = json_normalize(c_json, 'list')
 
 
 # 설정한 회사의 최근 공시 URL
+
 df = result[:10] #10을 고치는 것으로 갯수 조절 가능
 for ix, row in df.iterrows():
     url_tmpl = 'http://dart.fss.or.kr/dsaf001/main.do?rcpNo={}'
@@ -65,7 +73,54 @@ def get_report_doc_urls(rcp_no):
 
     return doc_urls
 
-doc_urls = get_report_doc_urls(get_rcp_no('005930')[0])
-print(len(doc_urls))
-for url in doc_urls:
-    print(url)
+#첨부파일 url 가져오기
+def get_report_attach_urls(rcp_no):
+    attach_urls = []
+
+    url = "http://dart.fss.or.kr/dsaf001/main.do?rcpNo=%s" % (rcp_no)
+    r = requests.get(url)
+
+    start_str = "javascript: viewDoc\('" + rcp_no + "', '"
+    end_str = "', null, null, null,"
+    reg = re.compile(start_str + '(.*)' + end_str)
+    m = reg.findall(r.text)
+    dcm_no = m[0]
+
+    attach_urls.append(pdf_link_tmpl.format(rcp_no=rcp_no, dcm_no=dcm_no))
+    attach_urls.append(excel_link_tmpl.format(rcp_no=rcp_no, dcm_no=dcm_no))
+    attach_urls.append(ifrs_link_tmpl.format(rcp_no=rcp_no, dcm_no=dcm_no))
+    return attach_urls
+
+rcp_no = '20170515003806'
+attach_urls = get_report_attach_urls(rcp_no)
+print(attach_urls)
+
+#pdf문서 저장하기
+def save_pdf():
+    url = attach_urls[0]
+    fname = '005930_' + rcp_no + '.pdf'
+
+    with open(fname, 'wb') as f:
+        f.write(requests.get(url).content)
+
+#엑셀문서 저장하기
+def save_excel():
+    url = attach_urls[1]
+    fname = '005930_' + rcp_no + '.xls'
+
+    with open(fname, 'wb') as f:
+        f.write(requests.get(url).content)
+
+#ZIP 문서 저장하기
+def save_zip():
+    url = attach_urls[2]
+    fname = '005930_' + rcp_no + '.zip'
+
+    with open(fname, 'wb') as f:
+        f.write(requests.get(url).content)
+
+#save_pdf()
+#save_excel()
+#save_zip()
+
+df = pd.read_excel('005930_20170515003806.xls', sheet_name = '연결 재무상태표', index_col=0, skiprows=6)
